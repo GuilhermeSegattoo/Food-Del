499adbb5654082df1aa0eb07d264fc60f4218d98
Guilherme Segatto <segattooguilherme@gmail.com>
2024-08-22 23:31:06 -0300

    Header, Cart, Home e PlaceOrder

---
+++ frontend/src/components/Header/Header.jsx
@@ -0,0 +1,15 @@
+import './Header.css'
+
+
+const Header = () => {
+    return (
+        <div className='header'>
+          <div className="header-contents">
+            <h2>Escolha sua comida favorita aqui!</h2>
+            <p>Escolha entre um menu diversificado com uma deliciosa variedade de pratos criados com os melhores ingredientes e experiência culinária</p>
+          </div>
+        </div>
+    )
+}
+
+export default Header
