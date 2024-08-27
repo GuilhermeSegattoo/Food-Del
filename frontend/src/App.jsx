9d5cb3dee230e2e4390f4541f92ed270776058db
Guilherme Segatto <segattooguilherme@gmail.com>
2024-08-26 22:15:26 -0300

    Adicionei o componente Footer junto com sua estilização

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,10 +3,12 @@ import Navbar from "./components/Navbar/Navbar"
 import Home from "./pages/Home/Home"
 import Cart from "./pages/Cart/Cart"
 import PlaceOrder from "./pages/PlaceOrder/PlaceOrder"
+import Footer from "./components/Footer/Footer"
 
 function App() {
   return (
-    <div>
+    <>
+     <div>
       <Navbar/>
       <Routes>
         <Route path="/" element={<Home/>}/>
@@ -14,6 +16,8 @@ function App() {
         <Route path="/order" element={<PlaceOrder/>}/>
       </Routes>
     </div>
+    <Footer/>
+    </>
   )
 }
 
